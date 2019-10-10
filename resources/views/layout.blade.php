ec4849519798f0c3cba7664ca7d81aa15a81ecff
nekotog47 <nekotog47@gmail.com>
2019-10-10 23:24:41 +0900

    @yield('styles')の場所変更＆reset.css追加

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Petgram</title>
-    @yield('styles')
+    <link rel="stylesheet" href="/css/Foundation/reset.css">
     <link rel="stylesheet" href="/css/Object/component/button.css">
     <link rel="stylesheet" href="/css/Object/component/grid.css">
     <link rel="stylesheet" href="/css/Object/component/modal.css">
     <link rel="stylesheet" href="/css/style.css">
+    @yield('styles')
 </head>
 <body>
 @include('header')
