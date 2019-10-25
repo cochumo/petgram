53471e33d2244a8207ba59c657f2f589502a7709
cochumo <stream99990@yahoo.co.jp>
2019-10-26 00:50:50 +0900

    bootstrapを消したからコーディングお手伝い...してたけどなんかbootstrap戻ったみたい

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,43 +12,45 @@
 @section('content')
     <div class="c-form__authWrap">
         <div class="c-form__authWrapInner">
-            <div class="u-textC">
-                <h1 class="c-form__auth--title">Petgram</h1>
+            <div class="">
+                <div class="c-form__auth--logo">
+                    <img src="{{ asset('img/logo-petgram.svg') }}" class="">
+                </div>
+                <form method="POST" action="{{ route('login') }}" class="c-form__authForm">
+                    @csrf
+                    <div class="c-form__authInput">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="ユーザーID (メールアドレス)">
+
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                             <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                    <div class="c-form__authInput">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="パスワード">
+
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                    <div class="c-form__authInput">
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                        <label class="form-check-label" for="remember">
+                            ログインしたままにする
+                        </label>
+                    </div>
+                    <div class="u-textC c-form__authInput">
+                        <button type="submit" class="c-button--03">
+                            ログイン
+                        </button>
+                    </div>
+                </form>
             </div>
-            <form method="POST" action="{{ route('login') }}" class="c-form__authForm">
-                @csrf
-                <div class="c-form__authInput">
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="ユーザーID (メールアドレス)">
-
-                    @error('email')
-                    <span class="invalid-feedback" role="alert">
-                         <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                </div>
-                <div class="c-form__authInput">
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="パスワード">
-
-                    @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-                </div>
-                <div class="c-form__authInput">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                    <label class="form-check-label" for="remember">
-                        ログインしたままにする
-                    </label>
-                </div>
-                <div class="u-textC">
-                    <button type="submit" class="c-button--03">
-                        ログイン
-                    </button>
-                </div>
-            </form>
-            <div class="u-textC">
+            <div class="c-form__registrationGuidance">
                 <p class="">アカウントをお持ちでない場合はこちらから</p>
                 <a href="{{ route('register') }}" class="c-button--02">新規登録</a>
             </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,46 +5,48 @@
         <img src="/img/logo-petgram.svg" alt="Petgram" class="c-form__logo">
     </header>
     <main class="c-form__authWrap">
-        <div class="c-form__authWrapInner">
-            <h1 class="c-form__authTtl">Petgramアカウントの新規登録</h1>
-            <form method="POST" action="{{ route('register') }}" class="c-form__authForm">
-                @csrf
-                <div class="c-form__authInput">
-                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="アカウント名">
-                    @error('name')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-                <div class="c-form__authInput">
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="ユーザーID（メールアドレス）">
+        <div class="c-form__authWrapInner--register">
+            <div class="">
 
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-                <div class="c-form__authInput">
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="パスワード">
+                <h1 class="c-form__authTtl">Petgramアカウントの新規登録</h1>
+                <form method="POST" action="{{ route('register') }}" class="c-form__authForm">
+                    @csrf
+                    <div class="c-form__authInput">
+                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="アカウント名">
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="c-form__authInput">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="ユーザーID（メールアドレス）">
 
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-                <div class="c-form__authInput">
-                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="パスワードの確認">
-                </div>
-                <div class="c-form__confirmBtnWrap">
-                    <button type="submit" class="c-button--02">確認へ進む</button>
-                </div>
-            </form>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="c-form__authInput">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="パスワード">
+
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="c-form__authInput">
+                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="パスワードの確認">
+                    </div>
+                    <div class="c-form__confirmBtnWrap">
+                        <button type="submit" class="c-button--02">確認へ進む</button>
+                    </div>
+                </form>
+            </div>
             <div class="u-textC">
-                <p class="">アカウントをお持ちの場合はこちらから</p>
-                <a href="{{ route('login') }}" class="c-button--03">ログインページ</a>
+                <a href="{{ route('login') }}" class="c-form__registrationGuidance--link">アカウントをお持ちの場合はこちらから</a>
             </div>
         </div>
     </main>
