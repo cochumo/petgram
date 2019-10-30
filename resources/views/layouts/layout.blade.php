d1e44848c5b4c3a4deaa37b3f91a0322f7d0f1ce
cochumo <stream99990@yahoo.co.jp>
2019-10-30 23:30:06 +0900

    defer追加

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Petgram</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="{{ asset('js/common.js') }}"></script>
+    <script src="{{ asset('js/common.js') }}" defer></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootflat/2.0.4/css/bootflat.min.css">
     <link rel="stylesheet" href="{{ asset('css/Foundation/reset.css') }}">
