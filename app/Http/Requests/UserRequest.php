bf0bfadda0e933de52e26a8eae3c76aeea1f5229
cochumo <stream99990@yahoo.co.jp>
2019-11-03 14:19:41 +0900

    使っていないので削除

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
