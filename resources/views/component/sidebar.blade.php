5089eddd68d116d026421ff878b904625e931404
PC-D17 <stream99990@yahoo.co.jp>
2019-11-07 12:57:55 +0900

    userテーブルにカラムを追加

--- resources/views/component/sidebar.blade.php
+++ resources/views/component/sidebar.blade.php
@@ -1,4 +1,12 @@
 <div id="slide_menu" class="l-sidebarWrap">
+    <div class="l-sidebar__userInfo">
+        <div class="l-sidebar__userInfo__img">
+            <img src="{{ asset('/img/default_thumbnail.svg') }}" alt="" class="p-detail__main__comment__img">
+        </div>
+        <div class="l-sidebar__userInfo__txt">
+            <p class="">{{ auth()->user()->name }}</p>
+        </div>
+    </div>
     <div class="l-sidebar__list">
         <a href="#" class="l-sidebar__btn">プロフィール編集</a>
     </div>
